c0bdd4ab3b229c955323214d93f457d29ab5878d
Anu Jain <anubhav.jain@live.in>
2021-05-05 00:10:55 -0700

    Add populating dropdowns for teams and scripts for adding schedules

--- src/index.js
+++ src/index.js
@@ -21,15 +21,61 @@ document.getElementById("createTeam").addEventListener("click", function () {
 });
 
 teamModalLocator.addEventListener("close", function () {
-  console.log(document.getElementById("teamName").value);
-  console.log(document.getElementById("cloudName").value);
-  console.log(document.getElementById("type").value);
+  var tn = document.getElementById("teamName").value;
+  var cn = document.getElementById("cloudName").value;
+  var type = document.getElementById("type").value;
   var teamBlock = {
-    "name": document.getElementById("teamName").value,
-    "cloud": document.getElementById("cloudName").value,
-    "type": document.getElementById("type").value
+    "name": tn,
+    "cloud": cn,
+    "type": type
   };
   json.team.push(teamBlock);
-  //console.log(JSON.stringify(json.team));
+  document.getElementById("serviceDef").value = JSON.stringify(json);
+});
+
+var addScheduleModalLocator = document.getElementById("addScheduleModal");
+
+document.getElementById("addSchedule").addEventListener("click", function () {
+  if (typeof addScheduleModalLocator.showModal === "function") {
+    
+    var teamDropdown = document.getElementById("teamNameDropdown");
+
+    for (var key1 in json.team) {
+      var currOption1 = document.createElement("option");
+      currOption1.setAttribute('value', json.team[key1].name);
+      currOption1.setAttribute('text', json.team[key1].name);
+      currOption1.innerHTML = json.team[key1].name;
+      teamDropdown.appendChild(currOption1);
+    }
+
+    var scriptDropdown = document.getElementById("scriptNameDropdown");
+
+    for (var key2 in json.scripts) {
+      var currOption2 = document.createElement("option");
+      currOption2.setAttribute('value', json.scripts[key2].name);
+      currOption2.setAttribute('text', json.scripts[key2].name);
+      currOption2.innerHTML = json.scripts[key2].name;
+      scriptDropdown.appendChild(currOption2);
+    }
+    
+    addScheduleModalLocator.showModal();
+  } else {
+    alert("The <dialog> API is not supported by this browser");
+  }
+});
+
+addScheduleModalLocator.addEventListener("close", function () {
+  
+
+  
+  var tn = document.getElementById("teamName").value;
+  var cn = document.getElementById("cloudName").value;
+  var type = document.getElementById("type").value;
+  var teamBlock = {
+    "name": tn,
+    "cloud": cn,
+    "type": type
+  };
+  json.team.push(teamBlock);
   document.getElementById("serviceDef").value = JSON.stringify(json);
 });
