69dc401b6d3630a591e127e85c708bb6edf9111f
Anu Jain <anubhav.jain@live.in>
2021-05-04 23:12:12 -0700

    First draft First draft with modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+var json = require("./metadata.json");
+document.getElementById("serviceDef").value = JSON.stringify(json);
+
+document.getElementById("copy").addEventListener("click", function () {
+  var copyText = document.getElementById("serviceDef");
+  copyText.select();
+  document.execCommand("copy");
+  alert("Copied to clipboard");
+});
+
+var teamModalLocator = document.getElementById("addTeamModal");
+
+document.getElementById("createTeam").addEventListener("click", function () {
+  if (typeof teamModalLocator.showModal === "function") {
+    teamModalLocator.showModal();
+  } else {
+    alert("The <dialog> API is not supported by this browser");
+  }
+});
+
+teamModalLocator.addEventListener("close", function () {
+  console.log(document.getElementById("teamName").value);
+  console.log(document.getElementById("cloudName").value);
+  console.log(document.getElementById("type").value);
+});
